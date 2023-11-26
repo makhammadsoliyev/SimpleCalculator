2bffa8c20daf7386da8bb777055cadfeb66068d0
makhammadsoliyev <makhammadsoliyev@gmail.com>
2023-11-26 21:48:01 +0500

    update program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string expression;
+decimal result;
+char[] separators = { '-', '+', '*', '/', '%', ':' };
+
+Console.WriteLine("Welcome to Simple Calculator!");
+expression = Console.ReadLine().Trim();
+
+string[] arrayOfExpression = expression.Split(separators, StringSplitOptions.TrimEntries);
+
+decimal firstNum = decimal.Parse(arrayOfExpression[0]);
+decimal secondNum = decimal.Parse(arrayOfExpression[1]);
+
+char theOperator = expression[expression.IndexOfAny(separators)];
+
+result = theOperator switch
+{
+    '+' => firstNum + secondNum,
+    '-' => firstNum - secondNum,
+    '/' => firstNum / secondNum,
+    ':' => firstNum / secondNum,
+    '%' => firstNum % secondNum,
+    '*' => firstNum * secondNum
+};
+
+Console.WriteLine(result);
